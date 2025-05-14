e5cec23f7b21817d2e8330ae9fcb70a9be7cc617
Orel Yosupov <Orely4242@gmail.com>
2025-05-14 13:53:43 +0300

    Gem2Gem: Add get&set methods for Position (Thanks Moran)

--- app/src/main/java/gem2gem/Position.java
+++ app/src/main/java/gem2gem/Position.java
@@ -1,10 +1,49 @@
 package gem2gem;
 
 public class Position {
-    private final int row;
-    private final int col;
-    public Position(int row, int col) {
-        this.row = row;
-        this.col = col;
+    private int x;
+    private int y;
+
+    public Position(int x, int y){
+        this.x = x;
+        this.y = y;
     }
+
+    public int getX() {
+        return this.x;
+    }
+
+    public int getY() {
+        return this.y;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        Position other = (Position) obj;
+        return x == other.x && y == other.y;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = x;
+        result = 31 * result + y;
+        return result;
+    }
+    
 }
