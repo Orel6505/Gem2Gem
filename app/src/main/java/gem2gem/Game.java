ec6d783502ca6b2ccbc7a4c2484faee6d6ffe801
Michal Kadusi <michal2@mail.sapir.ac.il>
2025-05-14 12:19:30 +0300

    checked if the board is valid

--- app/src/main/java/gem2gem/Game.java
+++ app/src/main/java/gem2gem/Game.java
@@ -14,11 +14,22 @@ public class Game
 		for (int row = 0; row < 10; row++) {
 			for (int col = 0; col < 10; col++) {
 				char c = (char) ('1' + rand.nextInt(5));
+
 				board[row][col] = c;
+				while(isNotValid(row,col,c)){
+					c = (char) ('1' + rand.nextInt(5));
+					board[row][col] = c;
+				}
 
 			}
 		}
 	}
+
+		public static boolean isNotValid(int row, int col,char c){
+			return (col >= 2 && board[row][col - 1] == c && board[row][col - 2] == c) ||
+			 (row >= 2 && board[row - 1][col] == c && board[row - 2][col] == c);
+		}
+
 
 	public static void printBoard()
 	{
