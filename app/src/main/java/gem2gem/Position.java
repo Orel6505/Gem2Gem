0b2985e09488dadfd94c9c1c9774808b4aee876e
moran121231 <morandahan75@gmail.com>
2025-05-14 13:49:14 +0300

    fixed

--- app/src/main/java/gem2gem/Position.java
+++ app/src/main/java/gem2gem/Position.java
@@ -1,5 +1,10 @@
 package gem2gem;
 
 public class Position {
-
+    private final int row;
+    private final int col;
+    public Position(int row, int col) {
+        this.row = row;
+        this.col = col;
+    }
 }
